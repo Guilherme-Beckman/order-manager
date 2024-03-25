f6a603a34d82be83852d6b780c205c3b519b6cdb
Guilherme-Beckman <guilhermebeckman3@gmail.com>
2024-03-24 21:01:01 -0400

    add base project generated by spring initializr

---
+++ spring-order-manager/src/main/java/com/beckman/ordermanager/OrderManagerApplication.java
@@ -0,0 +1,13 @@
+package com.beckman.ordermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderManagerApplication.class, args);
+	}
+
+}
